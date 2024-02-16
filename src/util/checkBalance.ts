f4cb3060d19028ca62af5412a57fa894fd043267
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-16 20:36:56 -0300

    update

--- src/util/checkBalance.ts
+++ src/util/checkBalance.ts
@@ -1,7 +1,7 @@
 import { detectFeatures } from "@thirdweb-dev/sdk";
 import {
   contractAddress,
-  erc1155TokenId,
+  erc721TokenId,
   minimumBalance,
 } from "const/yourDetails";
 
@@ -15,7 +15,7 @@ export default async function checkBalance(sdk, address) {
   const features = detectFeatures(contract.abi);
 
   if (features?.ERC1155?.enabled) {
-    balance = await contract.erc1155.balanceOf(address, erc1155TokenId);
+    balance = await contract.erc1155.balanceOf(address, erc721TokenId);
   } else if (features?.ERC721?.enabled) {
     balance = await contract.erc721.balanceOf(address);
   } else if (features?.ERC20?.enabled) {
