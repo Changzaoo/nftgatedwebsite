418bfabe17568422c0c5b22f316b9af7c5c1859b
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-16 20:12:06 -0300

    test

---
+++ const/yourDetails.ts
@@ -0,0 +1,11 @@
+// Replace this with your contract address
+export const contractAddress = "0x09F054c1F8BF1a57f46978baA89d593A118221B5";
+
+// Replace this with your domain name.
+export const domainName = "thirdweb-example.com";
+
+// Minimum number of tokens required to access
+export const minimumBalance = 1;
+
+// Token ID for ERC-1155 tokens
+export const erc721TokenId = 0;
