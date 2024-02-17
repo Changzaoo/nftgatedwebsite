ee6408c19e3da778ab686f1abdc35f3d94e63c11
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-17 00:02:28 -0300

    tes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -99,7 +99,7 @@ export async function getServerSideProps(context) {
   // Instantiate our SDK
   const sdk = ThirdwebSDK.fromPrivateKey(
     process.env.THIRDWEB_AUTH_PRIVATE_KEY,
-    "polygon",
+    "mumbai",
     { secretKey }
   );
 
