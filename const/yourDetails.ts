7a591564bfa1095c8c5abaa505156bc6306fa47c
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-16 23:01:02 -0300

    s

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -7,5 +7,5 @@ export const domainName = "thirdweb-example.com";
 // Minimum number of tokens required to access
 export const minimumBalance = 1;
 
-// Token ID for ERC-1155 tokens
+// Token ID for ERC-721 tokens
 export const erc721TokenId = 0;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="conteudo exclusivo para holder's do pass"
+          content="Learn how to use the thirdweb Auth SDK to create an NFT Gated Website"
         />
       </Head>
       <Component {...pageProps} />
