4f512227271b155c849089c25ad369dc42691091
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-20 03:13:11 -0300

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const contractAddress = "0x8e774E12A572d5B08EC1f149aA28FA4e56D764dE, 0x32C169fc9e8EcfBFe502b50D0ed197F4F5D8c6D4";
+export const contractAddress = "0x8e774E12A572d5B08EC1f149aA28FA4e56D764dE";
 
 
 // Replace this with your domain name.
