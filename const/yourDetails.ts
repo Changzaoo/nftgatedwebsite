fe267e81cd6b13e8ee1240d011d649a8c2337fc0
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-20 03:01:01 -0300

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,6 @@
 // Replace this with your contract address
-export const contractAddress = "0x8e774E12A572d5B08EC1f149aA28FA4e56D764dE";
+export const contractAddress = "0x8e774E12A572d5B08EC1f149aA28FA4e56D764dE, 0x32C169fc9e8EcfBFe502b50D0ed197F4F5D8c6D4";
+
 
 // Replace this with your domain name.
 export const domainName = "balde-gold.vercel.app/";
