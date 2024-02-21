93145a8b9f5dbc57b2a0a6f117a54960ba75a252
Vinicius <118037200+Changzaoo@users.noreply.github.com>
2024-02-21 00:43:01 -0300

    s

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -47,8 +47,6 @@ export default function Login() {
     src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x58d7015DDCF4f94A9204E5Bb8c98AA4F660a8B4C&chain=%7B%22name%22%3A%22Mumbai%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2F80001.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22maticmum%22%2C%22chainId%22%3A80001%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22mumbai%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fpolygon%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=44380f8db1180048ab61f8cf626bddd6&theme=light&primaryColor=purple"
     width="600px"
     height="600px"
-    style="max-width:100%;"
-    frameborder="0"
 ></iframe>
       </div>
         {contractMetadata && (
